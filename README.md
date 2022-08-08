00dd446715637f6ac4a87849e0e81f3f997a2658
StefanDruzdzel <sidruzdzel@gmail.com>
2022-08-08 13:19:05 -0600

    Initial commit, first submission to Udacity

---
+++ README.md
@@ -0,0 +1,48 @@
+# Sparkify ETL Project
+
+## Overview
+
+This is the first project in the Udacity Data Engineering Nano Degree Program.  
+The purpose is to build data pipelines that read in two types of json files and create a relational postgres database, consisting of 5 tables in a star schema.
+Spark is a fictional music streaming service.  The raw data files contain data on songs, and user listener data.  The goal of this project is to create a database organized for queries that analyze song plays.  
+
+Some of the code was provided as part of the project.  I provided all queries in sql_queries.py, and much of the code in etl.py (although the general layout and functions were provided by Udacity).
+
+## How to run
+1. To run locally, follow the instructions at the bottom of the README below to install a Docker image that will allow the postgresql DB to run
+https://github.com/kenhanscombe/project-postgres
+
+2. Run create_tables.py
+
+3. Run etl.py
+
+4. Run or alter test.py to view the contents of the databases
+
+
+## Schema Design
+
+The following schema design was provided to me as part of the course rubric:
+
+### Fact Table
+songplays - records in log data associated with song plays i.e. records with page NextSong
+    songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent
+
+### Dimension Tables
+users - users in the app
+    user_id, first_name, last_name, gender, level
+songs - songs in music database
+    song_id, title, artist_id, year, duration
+artists - artists in music database
+    artist_id, name, location, latitude, longitude
+time - timestamps of records in songplays broken down into specific units
+    start_time, hour, day, week, month, year, weekday
+    NOTE: the rubric specified "start_time", however that's redundant so I removed it
+        Additionally, this table does not have a way to be linked to the other tables.
+        I added in user_id
+        And I shuffled some code around to add in song_id as well.
+        So the final columns in the time table are:
+        user_id, hour, day, week, month, year, weekday, song_id
+
+### Other Notes
+1. If I was to devote more time to this, I would optimize the datatypes, researching whether smaller data types would be appropriate for some columns.
+2. I would also research and implement a way to insert multiple rows of data at a time since it would likely improve performance.
---
+++ sql_queries.py
@@ -0,0 +1,103 @@
+# DROP TABLES
+
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
+
+# CREATE TABLES
+
+songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays(
+    songplay_id SERIAL PRIMARY KEY,
+    start_time BIGINT,
+    user_id INT,
+    level TEXT,
+    song_id TEXT,
+    artist_id TEXT,
+    session_id INT,
+    location TEXT,
+    user_agent TEXT);
+""")
+
+user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users(
+    user_id INT PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT,
+    gender TEXT,
+    level TEXT);
+""")
+
+song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs(
+    song_id TEXT PRIMARY KEY,
+    title TEXT,
+    artist_id TEXT,
+    year NUMERIC,
+    duration NUMERIC);
+""")
+
+artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists(
+    artist_id TEXT PRIMARY KEY,
+    name TEXT,
+    location TEXT,
+    latitude NUMERIC,
+    longitude NUMERIC);
+""")
+
+time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time(
+    user_id BIGINT,
+    hour INT,
+    day INT,
+    week INT,
+    month INT,
+    year INT,
+    weekday INT,
+    song_id TEXT);
+""")
+
+# INSERT RECORDS
+
+songplay_table_insert = ("""INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+VALUES(%s, %s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING;
+""")
+
+user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level)
+VALUES(%s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING;
+""")
+
+song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration)
+VALUES(%s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING;
+""")
+
+artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude)
+VALUES(%s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING;
+""")
+
+
+time_table_insert = ("""INSERT INTO time (user_id, hour, day, week, month, year, weekday, song_id)
+VALUES(%s, %s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING;
+""")
+
+# FIND SONGS
+
+song_select = ("""
+SELECT songs.song_id, artists.artist_id
+FROM songs
+JOIN artists ON songs.artist_id = artists.artist_id
+WHERE songs.title = %s AND artists.name = %s;
+""")
+
+# QUERY LISTS
+
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
